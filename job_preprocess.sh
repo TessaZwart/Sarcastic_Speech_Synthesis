b84ed881bf7a074130a6399c59cf1af78665707b
TessaZwart <63647830+TessaZwart@users.noreply.github.com>
2023-07-15 14:00:30 +0200

    Update job_preprocess.sh

--- job_preprocess.sh
+++ job_preprocess.sh
@@ -8,9 +8,6 @@
 #SBATCH --gres=gpu:1
 
 source new_env/bin/activate
-#module load Python/3.8.16-GCCcore-11.2.0
-#module load Python/3.10.4-GCCcore-11.3.0
-#module load cuDNN/8.4.1.50-CUDA-11.7.0
 module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0
 
 pip3 install -r requirements.txt
